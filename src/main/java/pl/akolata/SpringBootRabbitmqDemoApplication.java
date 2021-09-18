5716e8aaead27c007b8d8a2c5a2d7890acd8dbf3
Aleksander Kołata <olek.tbg.konta@gmail.com>
2021-09-18 22:53:33 +0200

    init project using Spring initializer

---
+++ src/main/java/pl/akolata/SpringBootRabbitmqDemoApplication.java
@@ -0,0 +1,13 @@
+package pl.akolata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRabbitmqDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRabbitmqDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/akolata/SpringBootRabbitmqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.akolata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRabbitmqDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
